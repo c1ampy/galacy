f37795c6c1c424eb8f22e4774a0ec5e8d57dbfa3
Li Muyuan <3278693507@qq.com>
2025-12-08 20:38:43 +0800

    想要做一个菜单，但是还没学完，明天补充完，然后开始做把游戏对象在窗口里显示出来

--- source/main.cpp
+++ source/main.cpp
@@ -1,150 +1,72 @@
-//仿照b站上的视频，现在还不能发射子弹，也没有敌机
+/*这是一个菜单，鼠标悬停的时候可以高亮，但是我还没有学会鼠标命中测试和悬停高亮，我大概在明天补充完*/
 
-#include <conio.h>
+
+
 #include <graphics.h>
-#include <time.h>
-#include <stdio.h>
-#include <math.h>
-#include <Windows.h>
-#pragma	comment(lib,"Winmm.lib")
+#include <windows.h>
+#include <string.h>
 
-#define SCREEN_WIDTH 400
-#define SCREEN_HEIGH 800
+// 菜单的结构
+typedef struct MenuItem {
+    char text[64];
+    RECT rect;
+    int hovered;
+} MenuItem;
 
-#define PLANE_SIZE 50
-
-#define ENEMY_NUM 8
-
-#define ENEMY_SPEED 1.0
-
-#define  BULLET_NUM 10
-
-typedef struct pos
-{
-	int x;
-	int y;
-
-}POS;
-
-typedef struct plane
-{
-	POS planePos;
-	POS planeBullets[BULLET_NUM];
-	int bulletLen;
-	int bulletSpeed;
-}PLANE;
-
-PLANE myPlane;
-PLANE enemyPlanes[ENEMY_NUM];
-int enemyPlaneLen;
-static time_t startTime, endTime;
-IMAGE img[3];
-int score = 0;
-
-void initGame();
-void drawGame();
-void updateGame();
-
-
-
-void initGame()
-{
-	initgraph(SCREEN_WIDTH, SCREEN_HEIGH);
-	score = 0;
-
-	srand((unsigned)time(NULL));//随机数种子
-
-	myPlane.bulletLen = 0;
-	myPlane.bulletSpeed = 3;
-	myPlane.planePos = { SCREEN_WIDTH / 2 - PLANE_SIZE / 2,SCREEN_HEIGH - PLANE_SIZE };
-
-	enemyPlaneLen = 0;
-
-	startTime = time(NULL);
-
+//构造一个矩形
+static RECT MakeRect(int x, int y, int w, int h) {
+    RECT r; r.left = x; r.top = y; r.right = x + w; r.bottom = y + h; return r;
 }
-
-
-
-
-void drawGame()
-{
-	BeginBatchDraw();   //开始批量贴图
-	//背景
-	putimage(0, 0, &img[0]);
-	putimage(myPlane.planePos.x - PLANE_SIZE / 2, myPlane.planePos.y - PLANE_SIZE / 2, &img[2], SRCAND);
-
-	for (int i = 0; i < enemyPlaneLen; i++) {
-		putimage(enemyPlanes[i].planePos.x - PLANE_SIZE / 2, enemyPlanes[i].planePos.y - PLANE_SIZE / 2, &img[1]);
-
-		for (int i = 0; i < myPlane.bulletLen; i++)
-		{
-			solidcircle(myPlane.planeBullets[i].x, myPlane.planeBullets[i].y, PLANE_SIZE / 4);
-		}
-
-		RECT rect = { 0,PLANE_SIZE,SCREEN_WIDTH,SCREEN_HEIGH };
-		setbkmode(TRANSPARENT);           // 文字背景透明
-		settextcolor(WHITE);              // 设置文字颜色为白色
-		char str[64] = { 0 };
-		snprintf(str, sizeof(str), "分数: %d", score);
-		drawtext(str, &rect, DT_TOP | DT_CENTER);
-	}
-
-	EndBatchDraw();//结束批量贴图
+static int HitTest(const MenuItem* it, int x, int y) {
+    POINT pt; pt.x = x; pt.y = y; return ::PtInRect(&it->rect, pt) != 0;
 }
-
-
-void updateGame()
-{   //更改位置
-	if (GetAsyncKeyState('W') & 0x8000)
-	{
-		myPlane.planePos.y -= 2;
-	}
-	if (GetAsyncKeyState('S') & 0x8000)
-	{
-		myPlane.planePos.y += 2;
-	}
-	if (GetAsyncKeyState('A') & 0x8000)
-	{
-		myPlane.planePos.x -= 2;
-	}
-	if (GetAsyncKeyState('D') & 0x8000)
-	{
-		myPlane.planePos.x += 2;
-	}
-
-	//发射子弹，这里我没做出来，好像120行写错了
-
-	if (_kbhit())
-	{
-		if (_getch() == ' ')
-		{
-			if (enemyPlaneLen < BULLET_NUM) {
-				PlaySound("img/1.wav", NULL, SND_FILENAME | SND_ASYNC | SND_NOWAIT);
-				myPlane.planeBullets[enemyPlaneLen] = myPlane.planePos;
-				myPlane.bulletLen++;
-			}
-		}
-	}
+//绘制按钮
+static void DrawButton(const MenuItem* it) {
+    if (it->hovered) { setfillcolor(RGB(80, 160, 255)); setlinecolor(RGB(255, 255, 255)); settextcolor(RGB(255, 255, 255)); }
+    else { setfillcolor(RGB(40, 80, 160));  setlinecolor(RGB(200, 200, 200)); settextcolor(RGB(230, 230, 230)); }
+    solidrectangle(it->rect.left, it->rect.top, it->rect.right, it->rect.bottom);
+    rectangle(it->rect.left, it->rect.top, it->rect.right, it->rect.bottom);
+    setbkmode(TRANSPARENT);
+    settextstyle(24, 0, "宋体");
+    RECT tr = it->rect; drawtext(it->text, &tr, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+}
+//高亮
+static void UpdateHover(MenuItem* items, int count, int mouseX, int mouseY, int* selected) {
+    int anyHover = 0;
+    for (int i = 0; i < count; ++i) {
+        int h = HitTest(&items[i], mouseX, mouseY);
+        items[i].hovered = h;
+        if (h) { *selected = i; anyHover = 1; }
+    }
+    if (!anyHover) {
+        for (int i = 0; i < count; ++i) items[i].hovered = (i == *selected);
+    }
 }
 
 int main() {
+    const int W = 480, H = 360;
+    initgraph(W, H);
+    setbkmode(TRANSPARENT);
+
+    //菜单
 
 
-	loadimage(&img[0], "img/背景.png", SCREEN_WIDTH, SCREEN_HEIGH);
-	loadimage(&img[1], "img/敌机.png", PLANE_SIZE, PLANE_SIZE);
-	loadimage(&img[2], "img/飞机.png", PLANE_SIZE, PLANE_SIZE);
-
-	initGame();
-	while (1)
-	{
-		drawGame();
-		updateGame();
-		Sleep(16);
-	}
+    //初始状态
 
 
-	getchar();
-	getchar();
-	return 0;
+        //鼠标消息
+
+
+        //绘制
+    setfillcolor();
+    solidrectangle(0, 0, W, H);
+    for ()
+        DrawButton();
+    settextstyle);
+    settextcolor();
+    outtextxy();
+
+    FlushBatchDraw();
+    Sleep(16);
+}
 }
